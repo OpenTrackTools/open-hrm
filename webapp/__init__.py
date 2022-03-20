8dd4167b883ed6c7e62505404c16926d2e612212
Arpan Mukhopadhyay <arixion@outlook.com>
2022-03-20 21:32:57 +0530

    initial scaffolding

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,9 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_assets import Environment, Bundle
 
 db = SQLAlchemy()
 migrate = Migrate()
+asset_env = Environment()
+
+# TODO will be changed once the client application finalized
+
+main_css = Bundle(
+    'css/bootstrap.css',
+    filters='cssmin',
+    output='css/app.min.css'
+)
+
+main_js = Bundle(
+    'js/jquery.js',
+    'js/bootstrap.js',
+    filters='jsmin',
+    output='js/app.min.js'
+)
 
 
 def create_app(object_name):
@@ -11,6 +28,10 @@ def create_app(object_name):
     app.config.from_object(object_name)
     db.init_app(app)
     migrate.init_app(app, db)
+    
+    asset_env.register('js_all', main_js)
+    asset_env.register('css_all', main_css)
+    asset_env.init_app(app)
     
     from .authentication import create_module as auth_create_mod
     from .validation import create_module as validation_create_mod
